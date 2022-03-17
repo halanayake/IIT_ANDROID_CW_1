cea842122f5601ce43e37c8de8157819e1bd26f5
Gayath Halanayake <gayath29@ymail.com>
2022-03-18 00:54:06 +0530

    about done

--- app/src/main/java/com/example/equalequations/MainActivity.kt
+++ app/src/main/java/com/example/equalequations/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.example.equalequations
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.*
+import android.widget.LinearLayout
+import android.widget.PopupWindow
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
+    fun openAboutPopUp(view: View) {
+
+        // inflate the layout of the popup window
+        val inflater = getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val popupView: View = inflater.inflate(R.layout.about_popup, null)
+
+        // create the popup window
+        val width = LinearLayout.LayoutParams.MATCH_PARENT
+        val height = LinearLayout.LayoutParams.MATCH_PARENT
+        val focusable = true // lets taps outside the popup also dismiss it
+        val popupWindow = PopupWindow(popupView, width, height, focusable)
+
+        // show the popup window
+        // which view you pass in doesn't matter, it is only used for the window tolken
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0)
+
+        // dismiss the popup window when touched
+        popupView.setOnTouchListener { v, event ->
+            when (event?.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    v.performClick()
+                }
+            }
+            popupWindow.dismiss()
+            true
+        }
+
+    }
+
 }
