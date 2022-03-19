adb0c7309185df4464903b28cef2455a8679c423
Gayath Halanayake <gayath29@ymail.com>
2022-03-20 01:51:23 +0530

    basic done

--- app/src/main/java/com/example/equalequations/GameActivity.kt
+++ app/src/main/java/com/example/equalequations/GameActivity.kt
@@ -1,72 +1,75 @@
 package com.example.equalequations
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.PopupWindow
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import kotlin.math.roundToInt
 
 
 class GameActivity : AppCompatActivity() {
 
     class DataObj(val equation: String, var total: Int)
 
+    private var leftEquation:DataObj? = null
+    private var rightEquation:DataObj? = null
+
+    private val leftEqConst = "left_eq"
+    private val leftValConst = "left_val"
+    private val rightEqConst = "right_eq"
+    private val rightValConst = "right_val"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
-        generateLeftEquation()
-        generateRightEquation()
+        generateAndAssignEquations(savedInstanceState)
     }
 
-    private fun generateLeftEquation() {
-        val textView: TextView = findViewById(R.id.leftEquation)
-        val equation = generateEquation()
-        textView.text = equation.equation
-        Toast.makeText(this, "LEFT - " + equation.total, Toast.LENGTH_SHORT).show()
-        Log.e("GHX", "--------------------------------------------")
+    private fun generateAndAssignEquations(savedInstanceState: Bundle?) {
+        val tempLeftEq = savedInstanceState?.getString(leftEqConst)
+        val tempLeftVal = savedInstanceState?.getInt(leftValConst)
+        leftEquation = if (tempLeftEq != null && tempLeftVal != null) {
+            DataObj(tempLeftEq, tempLeftVal)
+        } else {
+            generateEquation()
+        }
+        val textViewLeft: TextView = findViewById(R.id.leftEquation)
+        textViewLeft.text = leftEquation!!.equation
+
+        val tempRightEq = savedInstanceState?.getString(rightEqConst)
+        val tempRightVal = savedInstanceState?.getInt(rightValConst)
+        rightEquation = if (tempRightEq != null && tempRightVal != null) {
+            DataObj(tempRightEq, tempRightVal)
+        } else {
+            generateEquation()
+        }
+        val textViewRight: TextView = findViewById(R.id.rightEquation)
+        textViewRight.text = rightEquation!!.equation
     }
-    private fun generateRightEquation() {
-        val textView: TextView = findViewById(R.id.rightEquation)
-        val equation = generateEquation()
-        textView.text = equation.equation
-        Toast.makeText(this, "RIGHT - " + equation.total, Toast.LENGTH_SHORT).show()
+
+    override fun onSaveInstanceState(state: Bundle) {
+        super.onSaveInstanceState(state)
+        if (leftEquation != null) {
+            state.putString(leftEqConst, leftEquation!!.equation)
+            state.putInt(leftValConst, leftEquation!!.total)
+        }
+        if (rightEquation != null) {
+            state.putString(rightEqConst, rightEquation!!.equation)
+            state.putInt(rightValConst, rightEquation!!.total)
+        }
     }
 
     private fun generateEquation(): DataObj {
-        /*val operators = arrayOf('+', '-', '*', '/')
-        var equation = ""
-        var total = 0
-        val termCount = (1..4).random()
-        for (term in (1..termCount)) {
-            var operator = 'x'
-            if (term != 1) {
-                val operatorPicker = (0..3).random()
-                operator = operators[operatorPicker]
-                equation += operator
-            }
-
-            var number = (1..20).random()
-
-            when (operator) {
-                'x' -> total += number
-                '+' -> total += number
-                '-' -> total -= number
-                '*' -> total *= number
-                '/' -> total /= fixDivision(total, number)
-            }
-
-            equation += number
-
-            if (term > 1 && termCount > 2 && term != termCount) {
-                equation = "($equation)"
-            }
-        }*/
-
         val operators = mutableListOf('+', '-', '*', '/')
-        var selectedNumbers = mutableListOf<Int>()
-        var selectedOperators = mutableListOf<Char>()
-
+        val selectedNumbers = mutableListOf<Int>()
+        val selectedOperators = mutableListOf<Char>()
         val termCount = (1..4).random()
         for (term in (1..termCount)) {
             selectedNumbers.add((1..20).random())
@@ -74,34 +77,31 @@ class GameActivity : AppCompatActivity() {
                 selectedOperators.add(operators.removeAt((0 until operators.size).random()))
             }
         }
-        if ('-' in selectedOperators) {
-            Log.e("GHX", "Before Sub - $selectedNumbers || $selectedOperators")
-            sortSubtraction(selectedNumbers, selectedOperators)
-            Log.e("GHX", "After Sub - $selectedNumbers || $selectedOperators")
-        }
-        if ('/' in selectedOperators) {
-            Log.e("GHX", "Before Div - $selectedNumbers || $selectedOperators")
-            sortDivision(selectedNumbers, selectedOperators)
-            Log.e("GHX", "After Div - $selectedNumbers || $selectedOperators")
-        }
-
-        Log.e("GHX", "Final - $selectedNumbers || $selectedOperators")
-
+        fixEquation(selectedNumbers, selectedOperators)
         var equation = ""
-        for ((index, op) in operators.withIndex()) {
-            // TODO fix this
-            if (selectedNumbers.size > 1) {
-                equation += "${selectedNumbers[index]}$op${selectedNumbers[index+1]}"
-            } else {
-                equation += "${selectedNumbers[index]}"
+        val total = solveEquationAndResolve(selectedNumbers, selectedOperators)
+        for ((index, num) in selectedNumbers.withIndex()) {
+            equation += num
+            if (index != 0 && index != selectedNumbers.lastIndex) {
+                equation = "($equation)"
+            }
+            if (index != selectedNumbers.lastIndex) {
+                equation += selectedOperators[index]
             }
         }
-        val total = solveEquation(selectedNumbers, selectedOperators)
         return DataObj(equation, total)
     }
 
-    private fun sortSubtraction(numbers: MutableList<Int>, operators: MutableList<Char>) {
-        Log.e("GHX", ">>In SUb - $numbers || $operators")
+    private fun fixEquation(selectedNumbers: MutableList<Int>, selectedOperators: MutableList<Char>) {
+        if ('-' in selectedOperators) {
+            fixSubtraction(selectedNumbers, selectedOperators)
+        }
+        if ('/' in selectedOperators) {
+            fixDivision(selectedNumbers, selectedOperators)
+        }
+    }
+
+    private fun fixSubtraction(numbers: MutableList<Int>, operators: MutableList<Char>) {
         val maxPos = numbers.indexOf(numbers.maxOrNull()!!)
         val opIndex = operators.indexOf('-')
         val tempVal1 = numbers[opIndex]
@@ -111,16 +111,14 @@ class GameActivity : AppCompatActivity() {
         var secondMaxPos = numbers.lastIndex
         for ((index, num) in numbers.withIndex()) {
             if (num > numbers[secondMaxPos] && index != opIndex) {
-                secondMaxPos = index;
+                secondMaxPos = index
             }
         }
         numbers[opIndex+1] = numbers[secondMaxPos]
         numbers[secondMaxPos] = tempVal2
-        Log.e("GHX", ">>Out Sub - $numbers || $operators")
     }
 
-    private fun sortDivision(numbers: MutableList<Int>, operators: MutableList<Char>) {
-        Log.e("GHX", ">>In Div - $numbers || $operators")
+    private fun fixDivision(numbers: MutableList<Int>, operators: MutableList<Char>) {
         val min: Int = numbers.minOrNull()!!
         val opIndex = operators.indexOf('/')
         val tempVal = numbers[opIndex+1]
@@ -138,10 +136,7 @@ class GameActivity : AppCompatActivity() {
             val removedNum = numbers.removeAt(mulIndex)
             operators.add('*')
             numbers.add(removedNum)
-            if ('-' in operators) {
-                sortSubtraction(numbers, operators)
-            }
-            sortDivision(numbers, operators)
+            fixEquation(numbers, operators)
         } else {
             while (true) {
                 var found = false
@@ -172,7 +167,6 @@ class GameActivity : AppCompatActivity() {
                 }
             }
         }
-        Log.e("GHX", ">>Out Div - $numbers || $operators")
     }
 
     private fun solveEquation(numbers: MutableList<Int>, operators: MutableList<Char>): Int {
@@ -181,7 +175,7 @@ class GameActivity : AppCompatActivity() {
             if (count == 0) {
                 total += number
             } else {
-                when (operators[count-1]) {
+                when (operators[count - 1]) {
                     '+' -> total += number
                     '-' -> total -= number
                     '*' -> total *= number
@@ -192,26 +186,135 @@ class GameActivity : AppCompatActivity() {
         return total
     }
 
-    /*private fun testSolveEquation(numbers: MutableList<Int>, operators: MutableList<Char>): Int {
-        var total = 0.0
-        for ((count, number) in numbers.withIndex()) {
-            if (count == 0) {
-                total += number
-            } else {
-                when (operators[count-1]) {
-                    '+' -> total += number
-                    '-' -> total -= number
-                    '*' -> total *= number
-                    '/' -> total /= number
+    private fun solveEquationAndResolve(numbers: MutableList<Int>, operators: MutableList<Char>): Int {
+        var total = 0
+        var flag: Boolean
+        Log.e("GHX", "$numbers || $operators")
+        while (true) {
+            flag = true
+            for ((count, number) in numbers.withIndex()) {
+                if (count == 0) {
+                    total += number
+                } else {
+                    when (operators[count - 1]) {
+                        '+' -> {
+                            if ((total + number) > 100) {
+                                numbers[numbers.indexOf(number)] =  (1..(100-total)).random()
+                                flag = false
+                                fixEquation(numbers, operators)
+                                break
+                            }
+                            total += number
+                        }
+                        '-' -> total -= number
+                        '*' -> {
+                            if ((total * number) > 100) {
+                                if ((100/total) == 0) {
+                                    numbers[numbers.indexOf(number)] =  1
+                                } else {
+                                    numbers[numbers.indexOf(number)] =  (1..(100/total)).random()
+                                }
+                                flag = false
+                                fixEquation(numbers, operators)
+                                break
+                            }
+                            total *= number
+                        }
+                        '/' -> total /= number
+                    }
                 }
             }
+            if (flag) {
+                break
+            }
         }
-        val intTot = total.toInt()
-        if ((total-intTot) != 0.0) {
-            Log.e("TEST", ">>>>> TEST FAIL - $numbers || $operators -> ${total-intTot}")
+        return total
+    }
+
+    fun onClickGreater(view: View) {
+        if (leftEquation!!.total > rightEquation!!.total) {
+            showCorrect(view)
         } else {
-            Log.w("TEST", ">>>>> TEST PASS - $numbers || $operators -> ${total-intTot}")
+            showIncorrect(view)
         }
-        return 0
-    }*/
+        generateAndAssignEquations(null)
+    }
+
+    fun onClickEqual(view: View) {
+        if (leftEquation!!.total == rightEquation!!.total) {
+            showCorrect(view)
+        } else {
+            showIncorrect(view)
+        }
+        generateAndAssignEquations(null)
+    }
+
+    fun onClickLess(view: View) {
+        if (leftEquation!!.total < rightEquation!!.total) {
+            showCorrect(view)
+        } else {
+            showIncorrect(view)
+        }
+        generateAndAssignEquations(null)
+    }
+
+    private fun showCorrect(view: View) {
+        val inflater = getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val popupView: View = inflater.inflate(R.layout.correct_feedback, null)
+        val width = LinearLayout.LayoutParams.MATCH_PARENT
+        val height = LinearLayout.LayoutParams.MATCH_PARENT
+        val focusable = true
+        val popupWindow = PopupWindow(popupView, width, height, focusable)
+
+        val handler = Handler(Looper.getMainLooper())
+        val myRunnable = Runnable {
+            Log.e("GHX", "Runnable EXECUTED")
+            popupWindow.dismiss()
+        }
+        handler.postDelayed(myRunnable, 2000)
+
+        popupWindow.setOnDismissListener {
+            handler.removeCallbacksAndMessages(null)
+        }
+
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0)
+        popupView.setOnTouchListener { v, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    v.performClick()
+                }
+            }
+            popupWindow.dismiss()
+            true
+        }
+    }
+
+    private fun showIncorrect(view: View) {
+        val inflater = getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val popupView: View = inflater.inflate(R.layout.incorrect_feedback, null)
+        val width = LinearLayout.LayoutParams.MATCH_PARENT
+        val height = LinearLayout.LayoutParams.MATCH_PARENT
+        val focusable = true
+        val popupWindow = PopupWindow(popupView, width, height, focusable)
+        val handler = Handler(Looper.getMainLooper())
+        val myRunnable = Runnable {
+            Log.e("GHX", "Runnable EXECUTED")
+            popupWindow.dismiss()
+        }
+        handler.postDelayed(myRunnable, 2000)
+        popupWindow.setOnDismissListener {
+            handler.removeCallbacksAndMessages(null)
+        }
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0)
+        popupView.setOnTouchListener { v, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    v.performClick()
+                }
+            }
+            popupWindow.dismiss()
+            true
+        }
+    }
+
 }
